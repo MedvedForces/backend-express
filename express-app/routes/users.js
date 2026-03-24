f7e610b918a8451059ff3c0ae13dae23ad4f15c7
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2026-03-24 18:14:19 +0500

    5 task

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+usersList = new Array(
+  {"id": 1,
+      "name": "Alexandr"},
+      {"id": 2,
+      "name": "Kristina"},
+      {"id": 3,
+      "name": "Pavel"},
+      {"id": 4,
+      "name": "Maximeeeeeeeeeeeeeeeeeeee"},
+);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    items : usersList
+  });
+});
+
+router.post('/', function(req, res, next){
+  const name = req.body.name;
+  const newIdUser = usersList.length;
+  const newUser = {
+    "id" : newIdUser,
+    name
+  };
+
+  usersList.push(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
